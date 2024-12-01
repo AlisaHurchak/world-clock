75120c24a9860bfa2c02a0b7e36b1bf6fdd2d878
Alisa Hurchak <alisahurchak@gmail.com>
2024-12-01 21:42:21 +0000

    Added functionality for select

--- javascript/index.js
+++ javascript/index.js
@@ -34,5 +34,25 @@ function updateTime() {
     "h:mm:ss [<small>]A[</small>]"
   );
 }
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city" >
+          <div class="city-list">
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
